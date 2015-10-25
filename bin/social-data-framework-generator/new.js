1524473a6093589f679c192ab70779af000d345e
troythewolfe <troythewolfe@gmail.com>
2015-10-24 20:04:37 -0700

    fleshing out default config and lang files

--- bin/social-data-framework-generator/new.js
+++ bin/social-data-framework-generator/new.js
@@ -6,86 +6,150 @@ var prompt = require('prompt');
 var argv = require('minimist')(process.argv.slice(2));
 var dirname = process.cwd();
 
+var USER_NAME = 'Your OpenID Username';
 var PROJECT_DIR = 'Project Directory';
 var SCHEMA_NAME = 'Schema Name';
 var SCHEMA_DESC = 'Schema Description';
-var DIR_STRUCTURE =  {
-    
+var OPEN_ID = 'Your Open ID';
+var SCHEMA_PARENT = 'Existing Schema (schema name or )';
+
+//TODO: After I sketch these out here, write schemas and then generate this directory structure from them
+var DIR_STRUCTURE = {
+
     "config" : [
+
+        //TODO: on validator, check to make sure a field only appears once
+
+        {
+            "name" : "users",
+            "type" : "json",
+            "content" : {
+                "users" : [{
+                    "username" : "", //unique identifier
+                    "id-system" : "openid", //system that id belongs to
+                    "role" : "admin" // roles have access to different parts of the workflows (admin, editor, researcher)
+                }]
+            }
+        },
+
         {
             "name" : "details",
             "type" : "json",
             "content" : {
-                "sections" : []
+                "sections" : [
+                    {
+                        "name" : "", //identifier for lang files, logging, etc
+                        "fields" : [""] //schema path to value
+                    }
+                ]
             }
-        }, 
+        },
 
+        //find a way to add dynamic overrides to default values (or layout values in general?)
         {
             "name" : "details-edit",
             "type" : "json",
-            "content" : {}      
-        }, 
+            "content" : {
+                
+                "required" : [{
+                    "document-state" : "new", //state change (new, suggest, pending, publish),
+                    "required" : [""] //schema path to value
+                }],
+                
+                "defaults" : [{
+                    'field' : "", //schema path to value,
+                    'value' : "" //default value for schema, must be valid for field type
+                }],
 
-        {
-            "name" : "infograpics",
-            "type" : "json",
-            "content" : {}      
-        }, 
+                "accept-public-suggestions" : "true", //public can suggest edits on existin records
+                "accept-public-new-records" : "true" //public can add new records into the system
+            }
+        },
 
         {
             "name" : "list",
             "type" : "json",
+            "per-page" : 10, //default number of entries to show per page by default (1, 5, 10, 20, 50, 100, 500, all)
             "content" : {
-                "fields" : [],
-            }      
-        }, 
+                "fields" : [{
+                    "field" : "", //schema path to value
+                    "size" : "", //size of cell (extra-small, small, medium, large, extra-large),
+                    "link" : true //is link to details
+                }]
+            }
+        },
 
         {
             "name" : "list-filters",
             "type" : "json",
             "content" :{
-                "filters" : [],
+                "sort-by" : [""], //fields to allow sorting by
+                "default-sort-by" : "",
+                "filters" : [{
+                    "field" : "", //schema path to value,
+                    "default-value" : ""
+                }]
             }
-        }, 
+        },
 
+        //TODO: Deal with these two later
         {
             "name" : "public-api",
             "type" : "json",
             "content" : {} 
-        }
-    ],
-    
-    "lang" : [
-
-        {
-            "name" : "details",
-            "type" : "json",
-            "content" : {
-                "page-title" : "Details",
-            }   
-        }, 
-
-        {
-            "name" : "details-edit",
-            "type" : "json",
-            "content" : {
-                "page-title" : "",
-            }
         },
 
         {
             "name" : "infograpics",
             "type" : "json",
+            "content" : {}
+        },
+    ],
+
+    "lang" : [
+
+        //this will need to map to the schema and provide localized values for the keys
+        {
+            "name" : "schema",
+            "type" : "json",
+            "content" : {}
+        },
+
+        {
+            "name" : "details",
+            "type" : "json",
             "content" : {
-                "page-title" : "Inforgraphics",
-            }   
-        }, 
+                
+                "page-title" : "Details",
+                
+                "sections" : [{
+                    "name" : "", //name of the section created in the config
+                    "title" : "" //translated title of that section
+                }]
+            
+            }
+        },
+
+        {
+            "name" : "details-edit",
+            "type" : "json",
+            "content" : {
+                "page-title" : "Edit Details",
+                "submit" : "Suggest Edits",
+                "help-text" : [{
+                    "field" : "", //schema path to value,
+                    "text" : "" //help text
+                }]
+            }
+        },
 
         {
             "name" : "list",
             "type" : "json",
             "content" : {
-                "page-title" : "",
+                "page-title" : "List of Records",
+                "total-entries" : "Total Entries",
+                "filter-button" : "Filter",
             }   
         }, 
 
@@ -97,20 +161,30 @@ var DIR_STRUCTURE =  {
             }   
         }, 
 
+        //TODO: Deal with these two later
         {
             "name" : "public-api",
             "type" : "json",
             "content" : {
                 "page-title" : "",
             }   
-        }
+        },
+        {
+            "name" : "infograpics",
+            "type" : "json",
+            "content" : {
+                "page-title" : "Inforgraphics",
+            }   
+        }, 
     ]
 };
 
 var SCHEMA_STUB = {
     "id" : null,
     "type" : "object",
-    "description" : ""
+    "description" : "",
+    "type" : "object",
+    "properties" : {}
 };
 
 if(argv.h){
@@ -122,22 +196,42 @@ function printHelp(){
     console.log('help is on the way!');
 }
 
-
 console.log('');
 console.log('Social Data Framework Generator');
 console.log('--------------------------------------');
 
 prompt.start();
 
-prompt.get([PROJECT_DIR, SCHEMA_NAME, SCHEMA_DESC], function (err, result) {
+prompt.get([USER_NAME, PROJECT_DIR, SCHEMA_NAME, SCHEMA_DESC], function (err, result) {
 
     makeContents(result, DIR_STRUCTURE);
+
+    console.log('');
+
+    if(!schemaNameExists()){
+
+        console.log('Schema "' + result[SCHEMA_NAME] + '"" does not exist');
+        console.log('');
+        console.log('Creating a unique schema.');
+        console.log('');
+        console.log('Using an existing schema is sometimes the best choice.');
+        console.log('For more information about creating a schema, type:');
+        console.log('$ sdfg-help schemas');
+    } else {
+        console.log('Schema "' + result[SCHEMA_NAME] + '"" exists');
+        console.log('To see the schema, type:');
+        console.log('$ sdfg-schemas THE_SCHEMA_NAME');
+    }
+
+    console.log('');
 });
 
-function instructions(){
-
+//TODO: look in schema directory for a matching schema
+function schemaNameExists(){
+    return true;
 }
 
+//TODO: Make this less noisey/nested, break it out into functions
 function makeContents(result, dirs){
 
     var currRoot = dirname + '/' +  result[PROJECT_DIR];
@@ -157,9 +251,10 @@ function makeContents(result, dirs){
         _.each(dirs[dir], function(file){
 
             if(dir === 'lang'){
+
                 file.type = 'en.' + file.type;
 
-                fs.writeFileSync(currDir + '/schema' + '.' + file.type, stringify({}), 'utf8');
+                fs.writeFileSync(currDir + '/' + file.name + '.' + file.type, stringify({}), 'utf8');
             }
 
             fs.writeFileSync(currDir + '/' + file.name + '.' + file.type, stringify(file.content), 'utf8');
@@ -170,21 +265,3 @@ function makeContents(result, dirs){
 function stringify(obj){
     return JSON.stringify(obj, null, 4); // Indented 4 spaces
 }
-
-
-/*
-console.log('Social Data Framework Generator (sdfg) : sdfg-new');
-
-console.dir(argv);
-
-if(argv.p){
-
-    var newDir = dirname + '/' + argv.p;
-
-    console.log(newDir);
-
-    mkdirp(newDir, function(err){
-        console.log(err);
-    });
-}
-*/
