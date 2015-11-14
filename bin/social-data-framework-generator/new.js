bcfedf6630fdf6f3c7a3711cc2dfd78514c47c57
troythewolfe <troythewolfe@gmail.com>
2015-11-13 22:22:03 -0800

    adding grunt file

--- bin/social-data-framework-generator/new.js
+++ bin/social-data-framework-generator/new.js
@@ -1,5 +1,11 @@
 #! /usr/bin/env node
 
+/**
+ * @module generator/new
+ * @description command line tool to generate new instance
+ * @param {string} -s - The name of the new schema and project (e.g. "my-data")
+ */
+
 var _ = require('underscore');
 var fs = require('fs');
 var mkdirp = require('mkdirp');
@@ -13,25 +19,6 @@ var USER_NAME = 'Your OpenID Username (leave blank or you can change later)';
 var PROJECT_DIR = 'Project Directory Name (required)';
 var SCHEMA_NAME = 'Schema Name (required)';
 var SCHEMA_DESC = 'Schema Description (leave blank or you can change later)';
-
-/*********************
-BUILD DIRECTORY OUTPUT STRUCTURE
-*/
-
-var DIR_STRUCTURE = {
-    "config" : [],
-    "lang" : [],
-    "data" : null
-};
-
-//set the config stubs to generate the new package
-addConfigs('details_edit.1');
-addConfigs('details.1');
-addConfigs('infographics.1');
-addConfigs('list_filters.1');
-addConfigs('list.1');
-addConfigs('api_public.1');
-addConfigs('system.1');
 
 var SCHEMA_STUB = {
     "id" : null,
@@ -76,27 +63,10 @@ function main(){
     GET INPUT
     */
 
-    makeContents(newSchema, DIR_STRUCTURE);
+    makeContents(newSchema);
 
     console.log('SUCCESS: Project package is available at ' + newSchema + '\n');
 
-}
-
-function setDefaultConfigValues(){
-
-    DIR_STRUCTURE.config['']
-
-}
-
-function addConfigs(schema){
-
-    DIR_STRUCTURE.config.push({
-
-        "name" : schema,
-        "type" : "json",
-        "content" : schemas['config-stubs'][schema]
-    
-    });
 }
 
 function printHelp(){
@@ -110,8 +80,46 @@ function schemaNameExists(){
     return true;
 }
 
-//TODO: Make this less noisey/nested, break it out into functions
-function makeContents(newSchema, dirs){
+function processPackage(stub, newSchema){
+
+    stub.name = stub.name + newSchema;
+    stub.description = stub.description + newSchema;
+
+    return stringify(stub);
+}
+
+/**
+ * @function makeContents
+ * @description Creates the new package and writes the files into it.
+ * @param {string} newSchema - The name of the new schema and project (e.g. "my-data")
+ */
+function makeContents(newSchema){
+
+    var dirs = {
+        "config" : [],
+        "lang" : [],
+        "data" : null
+    };
+
+    function addConfigs(schema){
+
+        dirs.config.push({
+
+            "name" : schema,
+            "type" : "json",
+            "content" : schemas['config-stubs'][schema]
+
+        });
+    }
+
+    //set the config stubs to generate the new package
+    addConfigs('details_edit.1');
+    addConfigs('details.1');
+    addConfigs('infographics.1');
+    addConfigs('list_filters.1');
+    addConfigs('list.1');
+    addConfigs('api_public.1');
+    addConfigs('system.1');
 
     var currRoot = dirname + '/' +  newSchema;
 
@@ -120,6 +128,10 @@ function makeContents(newSchema, dirs){
     SCHEMA_STUB['id'] = 'http://sdfg/' + newSchema + ".1";
 
     fs.writeFileSync(currRoot + '/schema.json', stringify(SCHEMA_STUB), 'utf8');
+
+    fs.writeFileSync(currRoot + '/main.js', schemas['config-stubs']['main'], 'utf8');
+
+    fs.writeFileSync(currRoot + '/package.json', processPackage(schemas['config-stubs']['package'], newSchema));
 
     _.each(Object.keys(dirs), function(dir){
         var currDir = currRoot + '/' + dir;
